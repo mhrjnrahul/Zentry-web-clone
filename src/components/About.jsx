a681830354353b60e70524c5b242e909c4db52f9
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-26 14:00:50 +0545

    created about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import React from "react";
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+    useGSAP(() =>{
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        })
 
-export default About
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0,
+        })
+    })
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relaticve mb-8 flex flex-col items-center gap-5">
+        <h2 className="font-[general] text-sm uppercase md:text-[10px]">
+          Welcome to zentry
+        </h2>
+
+        <div
+          className="mt-5 text-center text-4xl uppercase loading-[0.8]
+            md:text-[6rem]"
+        >
+          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure.
+        </div>
+
+        <div
+          className="about-subtext absolute bottom-[-80dvh] left-1/2 
+            w-full max-w-96 -translate-x-1/2 text-center font-circular-web text-lg md:max-w-[34rem]"
+        >
+          <p>The Game of Games begins-your life, now and epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms.</p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div
+          className="mask-clip-path about-image absolute left-1/2 top-0 z-20 h-[60vh] w-96 origin-center -translate-x-1/2 
+            overflow-hidden rounded-3xl md:w-[30vw]"
+        >
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
