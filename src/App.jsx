5087074dec5af27b9c9eec318ccd59ceb858d2ee
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-26 13:40:41 +0545

    created about.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
 
-      <section className="z-0 min-h-screen bg-blue" />
+      <About />
     </main>
   ) 
 }
