fed23d8797ad28089ec865b4cc6ab1204bc77fac
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-28 09:54:03 +0545

    modified duration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     gsap.to(navContainerRef.current, {
       y: isNavVisible ? 0 : -100,
       opacity: isNavVisible ? 1 : 0,
-      duration: 0.2,
+      duration: 0.5,
     })
   }, [isNavVisible])
 
