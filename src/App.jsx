9e35b98738f69ae5d08216b3c047e229e23372db
mhrjnrahul <maharjanrahul58@gmail.com>
2025-03-28 09:55:07 +0545

    added features.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,8 +8,8 @@ function App() {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
-
       <About />
+      <Features />
     </main>
   ) 
 }
